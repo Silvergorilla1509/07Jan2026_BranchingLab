0e44c57f26c3264c294c57ab2b20c22596af368b
Joey Jean Noel <227071007+Silvergorilla1509@users.noreply.github.com.>
2026-01-07 20:39:23 -0600

    Powerup featurebranch

---
+++ Powerup/Powerup.py
@@ -0,0 +1,11 @@
+first_number = input ("Enter the first number: ")
+second_number = input ("Enter the second number: ")
+
+num1 = int(first_number)
+num2 = int(second_number) 
+
+sum_value = num1 + num2
+
+print ("Thinking...")
+
+print(f"The sum of {num1} and {num2} is {sum_value}.")
